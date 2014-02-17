d0402b4c61e40eff696084f6879e26d1bc5d7007
semira27 <WJtJW3GT>
2014-02-17 21:47:04 +0100

    DATA

--- Service/Properties/AssemblyInfo.cs
+++ Service/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9d39ed6f-751b-488b-bfa6-9304add3e646")]
+[assembly: Guid("ec0dd8aa-a877-4d26-95bd-7dbd90a37a25")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
